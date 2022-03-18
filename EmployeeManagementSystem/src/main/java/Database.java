dbd25663906bf8feb771dc14a617feb96d81c3db
kwonhj0116 <k_hj0116@naver.com>
2022-03-18 16:52:51 +0900

    feat: add Database dummy class and dummy methods

---
+++ EmployeeManagementSystem/src/main/java/Database.java
@@ -0,0 +1,18 @@
+public class Database {
+    public Boolean add(String employeeNum) {
+        return true;
+    }
+
+    public Boolean del(String employeeNum) {
+        return true;
+    }
+
+    public Boolean sch(String employeeNum) {
+        return true;
+
+    }
+
+    public Boolean mod(String oldEmployeeNum, String newEmployeeNum) {
+        return true;
+    }
+}
