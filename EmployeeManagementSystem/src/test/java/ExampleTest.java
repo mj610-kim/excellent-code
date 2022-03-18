caafcdcc81a5c98886c740c2c5ad130ee335a21a
taemin00 <101778501+taemin00@users.noreply.github.com>
2022-03-18 11:24:29 +0900

    buid: Add dependency for mokito library
    Add dependency for mokito library

--- EmployeeManagementSystem/src/test/java/ExampleTest.java
+++ EmployeeManagementSystem/src/test/java/ExampleTest.java
@@ -1,9 +1,12 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.mockito.Mock;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 public class ExampleTest {
 
