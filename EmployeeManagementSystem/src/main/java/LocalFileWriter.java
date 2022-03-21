7c073c7510adcd9bade03b3e34e2ba5ec2533192
김명종 <mj610.kim@gmail.com>
2022-03-21 13:52:07 +0900

    feat: implement LocalFileWriter
    Implement LocalFileWriter to support opening, writing, and closing of
    local files. This commit passes all tests in LocalFileWriterTest.
    
    Signed-off-by: 김명종 <mj610.kim@gmail.com>

--- EmployeeManagementSystem/src/main/java/LocalFileWriter.java
+++ EmployeeManagementSystem/src/main/java/LocalFileWriter.java
@@ -1,6 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class LocalFileWriter implements OutputWriter {
 
     private final String outputFilePath;
+    private BufferedWriter bufferedWriter;
 
     public LocalFileWriter(String outputFilePath) {
         this.outputFilePath = outputFilePath;
@@ -8,17 +13,50 @@ public class LocalFileWriter implements OutputWriter {
 
     @Override
     public boolean open() {
-        return false;
+        if (bufferedWriter != null) {
+            System.out.println("File is already open...");
+            return false;
+        }
+
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(outputFilePath));
+        } catch (IOException e) {
+            System.out.println("Failed to open file...");
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public boolean close() {
-        return false;
+        if (bufferedWriter == null) {
+            System.out.println("File is not opened...");
+            return false;
+        }
+
+        try {
+            bufferedWriter.close();
+            bufferedWriter = null;
+        } catch (IOException e) {
+            System.out.println("IOException while trying to close file...");
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public boolean setNextLine(String line) {
-        return false;
+        try {
+            bufferedWriter.write(line);
+            bufferedWriter.write(System.lineSeparator());
+        } catch (IOException e) {
+            System.out.println("IOException while trying to write to file...");
+            return false;
+        }
+
+        return true;
     }
 
 }
