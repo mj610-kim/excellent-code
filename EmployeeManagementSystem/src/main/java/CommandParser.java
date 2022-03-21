3c104a2fb9b7a02e8badfde37af2cd2c57206683
박성진 (Seong-Jin Park) <101843332+seongjin-Park210@users.noreply.github.com>
2022-03-21 16:50:11 +0900

    Resolved all the conversation for CommandParser
    Changed all the methods to a static & modified test code

--- EmployeeManagementSystem/src/main/java/CommandParser.java
+++ EmployeeManagementSystem/src/main/java/CommandParser.java
@@ -2,31 +2,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CommandParser {
-    final int CMD_POS = 0;
-    final int OPT1_POS = 1;
-    final int OPT2_POS = 2;
-    final int OPT3_POS = 3;
+    final static int CMD_POS = 0;
+    final static int OPT1_POS = 1;
+    final static int OPT2_POS = 2;
+    final static int OPT3_POS = 3;
 
-    String[] cmdLine;
-    public void setCommandLine(String cmdLine){
-        this.cmdLine = cmdLine.split(",");
+    private static String[] parseCommandLine(String cmdLine){
+        return cmdLine.split(",");
     }
 
-    public String getCommand(){
-        return cmdLine[CMD_POS];
+    public static String getCommand(String cmdLine){
+        return parseCommandLine(cmdLine)[CMD_POS];
     }
 
-    public ArrayList<String> getOption(){
-        return new ArrayList<>(Arrays.asList(cmdLine).subList(OPT1_POS, OPT3_POS));
+    public static ArrayList<String> getOption(String cmdLine){
+        return new ArrayList<>(Arrays.asList(parseCommandLine(cmdLine)).subList(OPT1_POS, OPT3_POS));
     }
 
-    public Employee getEmployee() {
-        String employeeNum = cmdLine[4];
-        String name = cmdLine[5];
-        String cl = cmdLine[6];
-        String phoneNum = cmdLine[7];
-        String birthDay = cmdLine[8];
-        String certi = cmdLine[9];
+    public static Employee getEmployee(String cmdLine) {
+        String[] cmd = parseCommandLine(cmdLine);
+        String employeeNum = cmd[4];
+        String name = cmd[5];
+        String cl = cmd[6];
+        String phoneNum = cmd[7];
+        String birthDay = cmd[8];
+        String certi = cmd[9];
 
         Employee employee = new Employee(
                 employeeNum, name, cl, phoneNum, birthDay, certi
@@ -35,8 +35,9 @@ public class CommandParser {
         return employee;
     }
 
-    public ArrayList<String> getColumnData() {
-        return new ArrayList<>(Arrays.asList(cmdLine).subList(OPT3_POS+1, cmdLine.length));
+    public static ArrayList<String> getColumnData(String cmdLine) {
+        String[] cmd = parseCommandLine(cmdLine);
+        return new ArrayList<>(Arrays.asList(cmd).subList(OPT3_POS+1, cmd.length));
     }
 }
 
--- EmployeeManagementSystem/src/test/java/CommandParserTest.java
+++ EmployeeManagementSystem/src/test/java/CommandParserTest.java
@@ -17,57 +17,46 @@ public class CommandParserTest {
     String cmdLine3 = "MOD,-p,, ,name,KYUMOK KIM,name,KYUMOK LEE";
     String cmdLine4 = "MOD,-p,-m, ,birthday,09,cl,CL2";
 
-
-    CommandParser cmdParser1 = new CommandParser();
-    CommandParser cmdParser2 = new CommandParser();
-    CommandParser cmdParser3 = new CommandParser();
-    CommandParser cmdParser4 = new CommandParser();
-
-    @Before
-    public void setUp() {
-        cmdParser1.setCommandLine(cmdLine1);
-        cmdParser2.setCommandLine(cmdLine2);
-        cmdParser3.setCommandLine(cmdLine3);
-        cmdParser4.setCommandLine(cmdLine4);
-    }
+    @Mock
+    CommandParser cmdParser = mock(CommandParser.class);
 
     @Test
     public void getCommandTest() {
-        assertEquals("ADD", cmdParser1.getCommand());
-        assertEquals("DEL", cmdParser2.getCommand());
-        assertEquals("MOD", cmdParser3.getCommand());
-        assertEquals("MOD", cmdParser4.getCommand());
+        assertEquals("ADD", cmdParser.getCommand(cmdLine1));
+        assertEquals("DEL", cmdParser.getCommand(cmdLine2));
+        assertEquals("MOD", cmdParser.getCommand(cmdLine3));
+        assertEquals("MOD", cmdParser.getCommand(cmdLine4));
 
     }
 
     @Test
     public void getEmployeeTest() {
-        String birthday = cmdParser1.getEmployee().getBirthDayALL();
+        String birthday = cmdParser.getEmployee(cmdLine1).getBirthDayALL();
         assertEquals("19980906", birthday);
     }
 
     @Test
     public void getColumnDataTest() {
-        assertEquals("birthday", cmdParser2.getColumnData().get(0));
-        assertEquals("19900906", cmdParser2.getColumnData().get(1));
+        assertEquals("birthday", cmdParser.getColumnData(cmdLine2).get(0));
+        assertEquals("19900906", cmdParser.getColumnData(cmdLine2).get(1));
 
-        assertEquals("name", cmdParser3.getColumnData().get(0));
-        assertEquals("KYUMOK KIM", cmdParser3.getColumnData().get(1));
+        assertEquals("name", cmdParser.getColumnData(cmdLine3).get(0));
+        assertEquals("KYUMOK KIM", cmdParser.getColumnData(cmdLine3).get(1));
 
-        assertEquals("name", cmdParser3.getColumnData().get(2));
-        assertEquals("KYUMOK LEE", cmdParser3.getColumnData().get(3));
+        assertEquals("name", cmdParser.getColumnData(cmdLine3).get(2));
+        assertEquals("KYUMOK LEE", cmdParser.getColumnData(cmdLine3).get(3));
 
-        assertEquals("birthday", cmdParser4.getColumnData().get(0));
-        assertEquals("09", cmdParser4.getColumnData().get(1));
+        assertEquals("birthday", cmdParser.getColumnData(cmdLine4).get(0));
+        assertEquals("09", cmdParser.getColumnData(cmdLine4).get(1));
 
-        assertEquals("cl", cmdParser4.getColumnData().get(2));
-        assertEquals("CL2", cmdParser4.getColumnData().get(3));
+        assertEquals("cl", cmdParser.getColumnData(cmdLine4).get(2));
+        assertEquals("CL2", cmdParser.getColumnData(cmdLine4).get(3));
     }
 
     @Test
     public void getOptionTest() {
-        assertEquals("-p", cmdParser3.getOption().get(0));
-        assertEquals("-m", cmdParser4.getOption().get(1));
+        assertEquals("-p", cmdParser.getOption(cmdLine3).get(0));
+        assertEquals("-m", cmdParser.getOption(cmdLine4).get(1));
 
     }
 
