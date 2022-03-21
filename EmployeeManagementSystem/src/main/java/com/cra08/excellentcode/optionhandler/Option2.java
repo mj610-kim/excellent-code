fdae37eeca81f4805208cc4d3b9821772cb0b798
taemin00 <101778501+taemin00@users.noreply.github.com>
2022-03-21 19:58:16 +0900

    bug fix: handle null option in Option2.processOption()

--- EmployeeManagementSystem/src/main/java/com/cra08/excellentcode/optionhandler/Option2.java
+++ EmployeeManagementSystem/src/main/java/com/cra08/excellentcode/optionhandler/Option2.java
@@ -18,6 +18,10 @@ public class Option2 {
 
     public List<Employee> processOption(String option, List<Employee> employeeList, String column,
             String condition) throws IllegalArgumentException {
+
+        if (option.equals(""))
+            return employeeList;
+
         if (column.equals("name")) {
             if (!employeeNameFilter.checkValidOption(option)) {
                 throw new IllegalArgumentException("Invalid name option(" + option + ")");
