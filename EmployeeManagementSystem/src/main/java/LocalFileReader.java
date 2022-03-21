cbb09131fa58b6a034d568af139835edbd85965a
김명종 <mj610.kim@gmail.com>
2022-03-21 13:25:43 +0900

    feat: implement LocalFileReader
    Implement LocalFileReader so that opening, reading, and closing of local
    file for read works. This commit passes all tests in LocalFileReaderTest.
    
    Signed-off-by: 김명종 <mj610.kim@gmail.com>

--- EmployeeManagementSystem/src/main/java/LocalFileReader.java
+++ EmployeeManagementSystem/src/main/java/LocalFileReader.java
@@ -1,8 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class LocalFileReader implements InputReader {
 
-    String inputFilePath;
+    private final String inputFilePath;
+    private BufferedReader bufferedReader;
 
     public LocalFileReader(String inputFilePath) {
         this.inputFilePath = inputFilePath;
@@ -10,17 +14,42 @@ public class LocalFileReader implements InputReader {
 
     @Override
     public boolean open() {
-        return false;
+        if (bufferedReader != null) {
+            System.out.println("File is already open...");
+            return false;
+        }
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(inputFilePath));
+        } catch (FileNotFoundException e) {
+            System.out.println("Failed to open file...");
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public boolean close() {
-        return false;
+        if (bufferedReader == null) {
+            System.out.println("File is not opened...");
+            return false;
+        }
+
+        try {
+            bufferedReader.close();
+            bufferedReader = null;
+        } catch (IOException e) {
+            System.out.println("IOException while trying to close file...");
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public String getNextLine() throws IOException {
-        return null;
+        return bufferedReader.readLine();
     }
 
 }
