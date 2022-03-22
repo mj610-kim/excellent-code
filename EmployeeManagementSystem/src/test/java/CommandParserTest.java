350f2e069b40291fe277dd3c0a27d4f5ded7475e
김명종 <mj610.kim@gmail.com>
2022-03-23 02:11:29 +0900

    test: remove mock dependency for static method tests
    For mocking static methods, StaticMocks have to be used. Otherwise, the
    actual method can be tested. The purpose of the CommandParserTest is to
    test the actual behavior of CommandParser. So ther is no need to mock
    CommandParser.
    
    Signed-off-by: 김명종 <mj610.kim@gmail.com>

--- EmployeeManagementSystem/src/test/java/CommandParserTest.java
+++ EmployeeManagementSystem/src/test/java/CommandParserTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
 
 import com.cra08.excellentcode.CommandParser;
 import com.cra08.excellentcode.option.BirthMonthOption;
@@ -9,7 +8,6 @@ import com.cra08.excellentcode.option.PrintOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.Test;
-import org.mockito.Mock;
 
 public class CommandParserTest {
 
@@ -22,44 +20,41 @@ public class CommandParserTest {
     String invalidTwoColDataLen = "MOD,-p,, ,name,KYUMOK KIM,name";
     String invalidOneColDataLen = "DEL, , , ,birthday";
 
-    @Mock
-    CommandParser cmdParser = mock(CommandParser.class);
-
     @Test
     public void getCommandTest() {
-        assertEquals("ADD", cmdParser.getCommand(cmdLine1));
-        assertEquals("DEL", cmdParser.getCommand(cmdLine2));
-        assertEquals("MOD", cmdParser.getCommand(cmdLine3));
-        assertEquals("MOD", cmdParser.getCommand(cmdLine4));
-        assertThrows(IllegalArgumentException.class, () -> cmdParser.getCommand(invalidCmdLine));
+        assertEquals("ADD", CommandParser.getCommand(cmdLine1));
+        assertEquals("DEL", CommandParser.getCommand(cmdLine2));
+        assertEquals("MOD", CommandParser.getCommand(cmdLine3));
+        assertEquals("MOD", CommandParser.getCommand(cmdLine4));
+        assertThrows(IllegalArgumentException.class, () -> CommandParser.getCommand(invalidCmdLine));
     }
 
     @Test
     public void getEmployeeTest() {
-        String birthday = cmdParser.getEmployee(cmdLine1).getBirthDayAll();
+        String birthday = CommandParser.getEmployee(cmdLine1).getBirthDayAll();
         assertEquals("19980906", birthday);
-        assertThrows(IllegalArgumentException.class, () -> cmdParser.getEmployee(invalidEmpDataLength));
+        assertThrows(IllegalArgumentException.class, () -> CommandParser.getEmployee(invalidEmpDataLength));
     }
 
     @Test
     public void getColumnDataTest() {
-        assertEquals("birthday", cmdParser.getColumnData(cmdLine2).get(0));
-        assertEquals("19900906", cmdParser.getColumnData(cmdLine2).get(1));
+        assertEquals("birthday", CommandParser.getColumnData(cmdLine2).get(0));
+        assertEquals("19900906", CommandParser.getColumnData(cmdLine2).get(1));
 
-        assertEquals("name", cmdParser.getColumnData(cmdLine3).get(0));
-        assertEquals("KYUMOK KIM", cmdParser.getColumnData(cmdLine3).get(1));
+        assertEquals("name", CommandParser.getColumnData(cmdLine3).get(0));
+        assertEquals("KYUMOK KIM", CommandParser.getColumnData(cmdLine3).get(1));
 
-        assertEquals("name", cmdParser.getColumnData(cmdLine3).get(2));
-        assertEquals("KYUMOK LEE", cmdParser.getColumnData(cmdLine3).get(3));
+        assertEquals("name", CommandParser.getColumnData(cmdLine3).get(2));
+        assertEquals("KYUMOK LEE", CommandParser.getColumnData(cmdLine3).get(3));
 
-        assertEquals("birthday", cmdParser.getColumnData(cmdLine4).get(0));
-        assertEquals("09", cmdParser.getColumnData(cmdLine4).get(1));
+        assertEquals("birthday", CommandParser.getColumnData(cmdLine4).get(0));
+        assertEquals("09", CommandParser.getColumnData(cmdLine4).get(1));
 
-        assertEquals("cl", cmdParser.getColumnData(cmdLine4).get(2));
-        assertEquals("CL2", cmdParser.getColumnData(cmdLine4).get(3));
+        assertEquals("cl", CommandParser.getColumnData(cmdLine4).get(2));
+        assertEquals("CL2", CommandParser.getColumnData(cmdLine4).get(3));
 
-        assertThrows(IllegalArgumentException.class, () -> cmdParser.getColumnData(invalidTwoColDataLen));
-        assertThrows(IllegalArgumentException.class, () -> cmdParser.getColumnData(invalidOneColDataLen));
+        assertThrows(IllegalArgumentException.class, () -> CommandParser.getColumnData(invalidTwoColDataLen));
+        assertThrows(IllegalArgumentException.class, () -> CommandParser.getColumnData(invalidOneColDataLen));
     }
 
     @Test
