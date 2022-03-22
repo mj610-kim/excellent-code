03c4f57238a7ebaed35322f506ad3079cee4112f
김명종 <mj610.kim@gmail.com>
2022-03-22 10:37:10 +0900

    feat: add feature to verify file validity
    In the main method, a path to a system file or otherwise sensitive file
    may be given as the arguments. This can be problematic for the program.
    Apply secure coding principles and verify arguments before running
    program core.
    
    Signed-off-by: 김명종 <mj610.kim@gmail.com>

--- EmployeeManagementSystem/src/main/java/com/cra08/excellentcode/Main.java
+++ EmployeeManagementSystem/src/main/java/com/cra08/excellentcode/Main.java
@@ -1,6 +1,11 @@
 package com.cra08.excellentcode;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+
+    private static final String INPUT_OUTPUT_FILE_NAME_MATCHER = "^[a-z](?:_?[a-z0-9]+)*(\\.)(txt)$";
 
     public static void main(String[] args) {
         if (args.length != 2) {
@@ -11,8 +16,20 @@ public class Main {
 
         String inputFile = args[0];
         String outputFile = args[1];
+        if (!isFileNameAllowed(inputFile) || !isFileNameAllowed(outputFile)) {
+            System.out.println("Illegal file name");
+            System.out.println("Exiting...");
+            return;
+        }
 
         CommandHandler commandHandler = new CommandHandler(inputFile, outputFile);
         commandHandler.run();
     }
+
+    private static boolean isFileNameAllowed(String fileName) {
+        Pattern pattern = Pattern.compile(INPUT_OUTPUT_FILE_NAME_MATCHER, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(fileName);
+        return matcher.find();
+    }
+
 }
